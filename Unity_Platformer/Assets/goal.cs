294920f106aae46ba97f6170c0841252fcb09c0c
LockeKhushrenada <24321637+LockeKhushrenada@users.noreply.github.com>
2020-09-21 08:27:39 -0500

    Item Pickup

---
+++ Unity_Platformer/Assets/goal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goal : MonoBehaviour
+{
+    [SerializeField]
+    int goToLevel = 0;
+    int pickups;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pickups = GameObject.FindGameObjectsWithTag("pickup").Length;
+        if (pickups <= 0)
+        {
+            SceneManager.LoadScene(goToLevel);
+        }
+    }
+}
