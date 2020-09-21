c1ac13e36dd624496ef3a4659d756cd3afc5fa48
LockeKhushrenada <24321637+LockeKhushrenada@users.noreply.github.com>
2020-09-21 12:14:55 -0500

    Game Manager

--- Unity_Platformer/Assets/GameManager.cs
+++ Unity_Platformer/Assets/GameManager.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    public int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
